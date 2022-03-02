35e84c2ab34a2c37bfad7a419522e71577cef96b
MitchelMA <32548@ma-web.nl>
2022-03-02 14:40:53 +0100

    vector.h -> vector2.h wegens 2d; ook gebruikt het nu een namespace

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <iostream>
 #include <cmath>
-#include "vector.h"
+#include "vector2.h"
 
 using namespace std;
+using namespace MathVector;
 
 int main(void)
 {
      // instanciate a Vector with a given x and y
-     Vector testVector_one = Vector(1, 10);
+     Vector2 testVector_one = Vector2(1, 10);
      cout << "X value: " << testVector_one.getX() << endl
           << "Y value: " << testVector_one.getY() << endl
           << "Magnitude: " << testVector_one.mag() << endl
@@ -18,7 +19,7 @@ int main(void)
           << endl;
 
      // instanciate a Vector with a given angle in radians
-     Vector testVector_two = Vector(4);
+     Vector2 testVector_two = Vector2(4);
      cout << "X value: " << testVector_two.getX() << endl
           << "Y value: " << testVector_two.getY() << endl
           << "Magnitude: " << testVector_two.mag() << endl
